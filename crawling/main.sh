ac1ce49e653012032a00c04554626503d047a06e
maxim-lixakov <67663774+maxim-lixakov@users.noreply.github.com>
2022-03-05 20:25:46 +0300

    corrected configuration

--- crawling/main.sh
+++ crawling/main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+pwd
 cd spiders/yandex_and_google && scrapy crawl search --logfile log.log -o result_of_yandex_search.jl && cp -R /opt/app/spiders/yandex_and_google/search.jl /opt/app/spiders/cctld/search.jl && cd /opt/app/
 cd spiders/cctld && scrapy crawl whois --logfile log.log -o result_of_whois.jl && rm search.jl && cp -R /opt/app/spiders/cctld/result_of_whois.jl /opt/app/spiders/egrul/result_of_whois.jl && cd /opt/app/
 cd spiders/egrul && scrapy crawl egrul_info --logfile log.log -o result_of_egrul.jl && rm result_of_whois.jl && cp -R /opt/app/spiders/egrul/result_of_egrul.jl /opt/app/spiders/rusprofile/result_of_egrul.jl && cd /opt/app/
